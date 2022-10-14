ba611cf8a71ffe781beb634e46f4bb3b01bd3ae5
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-10-14 15:37:04 +0530

    LoginViewModel.kt change variable name taskResponseLiveData and add errorLiveData

--- app/src/main/java/com/guruthedev/instagram/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/guruthedev/instagram/viewModel/LoginViewModel.kt
@@ -1,4 +1,4 @@
-package com.guruthedev.instagram.ui.fragments.auth.login
+package com.guruthedev.instagram.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,18 +7,27 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 
-class LoginViewModel : ViewModel(){
+class LoginViewModel : ViewModel() {
 
     private val firebaseAuth = FirebaseAuth.getInstance()
     private val _taskResponse = MutableLiveData<Task<AuthResult>>()
-    val taskResponse: LiveData<Task<AuthResult>>
+    val taskResponseLiveData: LiveData<Task<AuthResult>>
         get() = _taskResponse
+    private val _errorMutableLiveData = MutableLiveData<Task<AuthResult>>()
+    val errorLiveData: LiveData<Task<AuthResult>>
+        get() = _errorMutableLiveData
 
-    fun login(email: String, password: String) {
+    private fun login(email: String, password: String) {
         firebaseAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { taskResult ->
                 _taskResponse.postValue(taskResult)
+                _errorMutableLiveData.postValue(taskResult)
             }
     }
 
+    fun validateCred(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            login(email, password)
+        }
+    }
 }
