3602737e6a914f63f42cb57d55c8f8750d182919
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-10-14 17:21:12 +0530

    In these ViewModel  added if else condition SignUpViewModel.kt

--- app/src/main/java/com/guruthedev/instagram/viewModel/SignUpViewModel.kt
+++ app/src/main/java/com/guruthedev/instagram/viewModel/SignUpViewModel.kt
@@ -9,24 +9,29 @@ import com.google.firebase.auth.FirebaseAuth
 
 class SignUpViewModel() : ViewModel() {
     private val firebaseAuth = FirebaseAuth.getInstance()
-    private val _taskResponse = MutableLiveData<Task<AuthResult>>()
+    private val _taskResponseLiveData = MutableLiveData<Task<AuthResult>>()
     val taskResponseLiveData: LiveData<Task<AuthResult>>
-        get() = _taskResponse
-    private val _errorMutableLiveData = MutableLiveData<Task<AuthResult>>()
-    val errorLiveData: LiveData<Task<AuthResult>>
-        get() = _errorMutableLiveData
+        get() = _taskResponseLiveData
+    private val _errorLiveData = MutableLiveData<String>()
+    val errorLiveData: LiveData<String>
+        get() = _errorLiveData
 
     private fun signUp(email: String, password: String) {
         firebaseAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { taskResult ->
-                _taskResponse.postValue(taskResult)
-                _errorMutableLiveData.postValue(taskResult)
+                if (taskResult.isSuccessful) {
+                    _taskResponseLiveData.postValue(taskResult)
+                } else {
+                    _errorLiveData.postValue(taskResult.exception?.message)
+                }
             }
     }
 
     fun validateCred(fullName: String, username: String, email: String, password: String) {
         if (fullName.isNotEmpty() && username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
             signUp(email, password)
+        } else {
+            _errorLiveData.postValue("Empty fields are not allowed")
         }
     }
 }
