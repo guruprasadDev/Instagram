73955751a8e0a21ea811da02b43bb7ff35fb6c90
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-10-27 10:45:31 +0530

    add strings in strings.xml

--- app/src/main/java/com/guruthedev/instagram/MainActivity.kt
+++ app/src/main/java/com/guruthedev/instagram/MainActivity.kt
@@ -31,6 +31,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun navigateTo(actionId: Int) {
+        findNavController(R.id.main_nav_host_container).navigate(actionId)
+    }
+
+    fun updateBottomNavVisibility(show: Boolean) {
+        binding.navView.visibility = if (show) VISIBLE else GONE
+    }
+
     private fun updatedPendingIntent() {
         val intent = Intent(this, MainActivity::class.java)
         val pendingIntent = TaskStackBuilder.create(this).run {
@@ -41,8 +49,8 @@ class MainActivity : AppCompatActivity() {
             )
         }
         val notify = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Instagram")
-            .setContentText("its_me_guru_reddy just shared a post")
+            .setContentTitle(getString(R.string.context_title))
+            .setContentText(getString(R.string.context_text))
             .setSmallIcon(R.drawable.insta)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(pendingIntent)
@@ -53,15 +61,6 @@ class MainActivity : AppCompatActivity() {
             notificationManager.notify(NOTIF_ID, notify)
         }
     }
-
-    fun navigateTo(actionId: Int) {
-        findNavController(R.id.main_nav_host_container).navigate(actionId)
-    }
-
-    fun updateBottomNavVisibility(show: Boolean) {
-        binding.navView.visibility = if (show) VISIBLE else GONE
-    }
-
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
