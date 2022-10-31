59eb931769c14d8e9793dfb9071bb927f4b22a52
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-10-31 17:58:39 +0530

    Implement GalleryFragment.kt

--- app/src/main/java/com/guruthedev/instagram/ui/fragments/GalleryFragment.kt
+++ app/src/main/java/com/guruthedev/instagram/ui/fragments/GalleryFragment.kt
@@ -1,15 +1,19 @@
 package com.guruthedev.instagram.ui.fragments
 
+import android.Manifest.permission.*
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.guruthedev.instagram.databinding.FragmentGalleryBinding
-import com.guruthedev.instagram.ui.adapter.GalleryAdapter
-import java.io.File
 
-class GalleryFragment:Fragment() {
+class GalleryFragment : Fragment() {
 
     private lateinit var binding: FragmentGalleryBinding
 
@@ -24,13 +28,49 @@ class GalleryFragment:Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-       loadGalleryImage()
+        binding.imagePicker.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val permissions = arrayOf(READ_EXTERNAL_STORAGE);
+                requestPermissions(permissions, PERMISSION_CODE)
+            } else {
+                pickImageFromGallery();
+            }
+        }
     }
 
-    private fun loadGalleryImage() {
-        val directory = File(context?.externalMediaDirs?.get(0)?.absolutePath ?: "")
-        val files = directory.listFiles() as Array<File>
-        val adapter = GalleryAdapter(files.reversedArray())
-        binding.viewPager.adapter = adapter
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    companion object {
+        private val IMAGE_PICK_CODE = 1000;
+        private val PERMISSION_CODE = 1001;
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    //permission from popup granted
+                    pickImageFromGallery()
+                } else {
+                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            binding.imagePicker.setImageURI(data?.data)
+        }
     }
 }
