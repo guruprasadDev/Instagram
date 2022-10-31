ba5bd363100d837f9e650b92fc90128f0a1c9439
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-10-31 17:59:49 +0530

    Add notification and ConnectivityManager

--- app/src/main/java/com/guruthedev/instagram/MainActivity.kt
+++ app/src/main/java/com/guruthedev/instagram/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.guruthedev.instagram
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.TaskStackBuilder
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.annotation.RequiresApi
@@ -16,6 +23,7 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.guruthedev.instagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,12 +32,46 @@ class MainActivity : AppCompatActivity() {
     val CHANNEL_NAME = "channelName"
     val NOTIF_ID = 0
 
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         createNotificationChannel()
         updatedPendingIntent()
+
+        val cm: ConnectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val builder: NetworkRequest.Builder = NetworkRequest.Builder()
+
+        cm.registerNetworkCallback(
+            builder.build(),
+            object : ConnectivityManager.NetworkCallback() {
+
+                @SuppressLint("ShowToast")
+                override fun onAvailable(network: Network) {
+                    Log.i("MainActivity", "onAvailable!")
+
+                    // check if NetworkCapabilities has TRANSPORT_WIFI
+                    val isWifi: Boolean = cm.getNetworkCapabilities(network)?.hasTransport(
+                        NetworkCapabilities.TRANSPORT_WIFI
+                    ) == true
+
+                    // doSomething()
+                }
+
+                override fun onLost(network: Network) {
+                    Log.i("MainActivity", "onLost!")
+                    val snack = Snackbar.make(
+                        binding.container,
+                        "Connect to Internet",
+                        Snackbar.LENGTH_LONG
+                    )
+                    snack.show()
+                    // doSomething
+                }
+            }
+        )
     }
 
     fun navigateTo(actionId: Int) {
@@ -59,9 +101,9 @@ class MainActivity : AppCompatActivity() {
             .build()
         val notificationManager = NotificationManagerCompat.from(this)
 
-        binding.notificationBtn.setOnClickListener {
-            notificationManager.notify(NOTIF_ID, notify)
-        }
+//        binding.notificationBtn.setOnClickListener {
+//            notificationManager.notify(NOTIF_ID, notify)
+//        }
     }
 
     private fun createNotificationChannel() {
@@ -76,8 +118,6 @@ class MainActivity : AppCompatActivity() {
             }
             val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             manager.createNotificationChannel(channel)
-
         }
     }
-
 }
