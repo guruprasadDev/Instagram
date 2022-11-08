b4c0ba4bb0e9144a40642a3aa37c748e9e507f0a
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-11-08 20:51:14 +0530

    Add Constants and scope function

--- app/src/main/java/com/guruthedev/instagram/AccountSettingsActivity.kt
+++ app/src/main/java/com/guruthedev/instagram/AccountSettingsActivity.kt
@@ -7,27 +7,37 @@ import androidx.databinding.DataBindingUtil
 import com.guruthedev.instagram.data.ResultData
 import com.guruthedev.instagram.databinding.ActivityAccountSettingsBinding
 import com.guruthedev.instagram.service.PassingData
+import com.guruthedev.instagram.utils.Constants
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
 class AccountSettingsActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityAccountSettingsBinding
+    private lateinit var binding: ActivityAccountSettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_account_settings)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_account_settings)
         EventBus.getDefault().register(this)
         binding.addBtn.setOnClickListener {
-                val intent = Intent(this,PassingData::class.java)
-                intent.putExtra("full_name",binding.fullNameProfileFrag.text.toString())
-                intent.putExtra("username",binding.usernameProfileFrag.text.toString())
-                intent.putExtra("bio",binding.bioProfileFrag.text.toString())
+            val intent = Intent(this, PassingData::class.java)
+            intent.apply {
+                putExtra(Constants.FULL_NAME, binding.fullNameProfileFrag.text.toString())
+                putExtra(Constants.USERNAME, binding.usernameProfileFrag.text.toString())
+                putExtra(Constants.BIO, binding.bioProfileFrag.text.toString())
                 startService(intent)
+            }
         }
     }
-    @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
-    fun onEvent(resultData: ResultData){
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onEvent(resultData: ResultData) {
+        binding.apply {
+            tvFullName.text = resultData.fullName
+            tvUsername.text = resultData.username
+            tvBio.text = resultData.bio
+        }
     }
+
     override fun onStop() {
         super.onStop()
         EventBus.getDefault().unregister(this)
