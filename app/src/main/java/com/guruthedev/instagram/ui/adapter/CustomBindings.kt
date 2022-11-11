897ae9f11608e3314590cdc554c22e7502aa9fe6
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-11-11 10:03:16 +0530

    create new function and add Glide features circleCrop

--- app/src/main/java/com/guruthedev/instagram/ui/adapter/CustomBindings.kt
+++ app/src/main/java/com/guruthedev/instagram/ui/adapter/CustomBindings.kt
@@ -3,10 +3,32 @@ package com.guruthedev.instagram.ui.adapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.guruthedev.instagram.R
 
 @BindingAdapter("loadImage")
 fun bindingImage(userImageView:ImageView,imageUri:String){
     Glide.with(userImageView.context)
         .load(imageUri)
+        .placeholder(R.drawable.instagram)
+        .error(R.drawable.error)
+        .fitCenter()
+        .apply(RequestOptions.circleCropTransform())
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .onlyRetrieveFromCache(true)
         .into(userImageView)
+}
+@BindingAdapter("loadImageUrl")
+fun bindingImageLogo(userImageView:ImageView,imageUri:String){
+    Glide.with(userImageView.context)
+        .load(imageUri)
+        .placeholder(R.drawable.instagram)
+        .error(R.drawable.error)
+        .fitCenter()
+        .circleCrop()
+        .apply(RequestOptions.circleCropTransform())
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .into(userImageView)
+
 }
