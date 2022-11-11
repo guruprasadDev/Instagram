c301c690ac8752f05518624c705a121fe4e99ec3
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-11-11 09:11:40 +0530

    Change Data class adding new data

--- app/src/main/java/com/guruthedev/instagram/data/Post.kt
+++ app/src/main/java/com/guruthedev/instagram/data/Post.kt
@@ -1,3 +1,5 @@
 package com.guruthedev.instagram.data
 
-data class Post(var id: Int, var name: String, var logo: String, var photo: String, var likes: String, var description: String)
+data class Post(val items: ArrayList<RecyclerData>)
+data class RecyclerData(val name: String, val description: String,val owner: Owner)
+data class Owner(val avatar_url:String)
