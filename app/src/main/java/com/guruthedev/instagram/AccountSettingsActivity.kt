e8009a79444ee38a1b9a69fc5997eb7991a50d21
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-11-15 12:58:27 +0530

    removed un used imports

--- app/src/main/java/com/guruthedev/instagram/AccountSettingsActivity.kt
+++ app/src/main/java/com/guruthedev/instagram/AccountSettingsActivity.kt
@@ -1,16 +1,9 @@
 package com.guruthedev.instagram
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.guruthedev.instagram.data.MessageEvent
 import com.guruthedev.instagram.databinding.ActivityAccountSettingsBinding
-import com.guruthedev.instagram.service.PassingData
-import com.guruthedev.instagram.utils.Constants
-import org.greenrobot.eventbus.EventBus
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
 
 class AccountSettingsActivity : AppCompatActivity() {
     private lateinit var binding:ActivityAccountSettingsBinding
