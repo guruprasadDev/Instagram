de3c8f082c0ab3453aa2f493d0f9488182360171
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-11-15 13:02:52 +0530

    observe live data for checking isConnectedToInternet

--- app/src/main/java/com/guruthedev/instagram/IgApplication.kt
+++ app/src/main/java/com/guruthedev/instagram/IgApplication.kt
@@ -1,29 +1,27 @@
 package com.guruthedev.instagram
 
 import android.app.Application
-import android.content.Context
 import com.guruthedev.instagram.data.pref.IgPreference
 import com.guruthedev.instagram.network.NetworkSyncer
 
 class IgApplication : Application() {
     private lateinit var preferences: IgPreference
+    var isConnectedToInternet: Boolean = false
 
     companion object {
         lateinit var instances: IgApplication
             private set
-
-        fun applicationContext(): Context {
-            return instances.applicationContext
-        }
     }
 
     override fun onCreate() {
         super.onCreate()
         instances = this
         preferences = IgPreference(this)
-        val context: Context = applicationContext()
-        val networkSyncer = NetworkSyncer(context)
+        val networkSyncer = NetworkSyncer(applicationContext)
         networkSyncer.start()
+        networkSyncer.currentNetworkState.observeForever { networkState ->
+            isConnectedToInternet = networkState.isConnected
+        }
     }
 
     fun getPreference(): IgPreference = preferences
