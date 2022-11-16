bb75cc6b277d20cc00e40f79689496efa8f0bf01
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-11-16 19:34:51 +0530

    Moved database to ViewModel

--- app/src/main/java/com/guruthedev/instagram/ui/fragments/ReelsFragment.kt
+++ app/src/main/java/com/guruthedev/instagram/ui/fragments/ReelsFragment.kt
@@ -5,40 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.guruthedev.instagram.data.Reels
 import com.guruthedev.instagram.databinding.FragmentReelsBinding
 import com.guruthedev.instagram.ui.adapter.ReelsAdapter
+import com.guruthedev.instagram.viewModel.ReelsFragmentViewModel
 
 class ReelsFragment : Fragment() {
     private lateinit var binding: FragmentReelsBinding
     private lateinit var reelsAdapter: ReelsAdapter
+    private lateinit var viewModel: ReelsFragmentViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentReelsBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(ReelsFragmentViewModel::class.java)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initDatabase()
-
+        initView()
     }
 
-    private fun initDatabase() {
-        val dataBase = Firebase.database.getReference("videos")
-        val options = FirebaseRecyclerOptions.Builder<Reels>()
-            .setQuery(dataBase, Reels::class.java)
-            .build()
-
-        reelsAdapter = ReelsAdapter(options)
+    private fun initView() {
+        reelsAdapter = ReelsAdapter(viewModel.getOptionsFromDatabase())
         binding.viewPager2.adapter = reelsAdapter
     }
+
 
     override fun onStart() {
         super.onStart()
