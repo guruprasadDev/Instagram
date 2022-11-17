8c2c77b67fac11274cb0363d3a69c99dc7851b5a
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2022-11-17 16:29:22 +0530

    removed the global assignment of binding

--- app/src/main/java/com/guruthedev/instagram/ui/adapter/FeedPostAdapter.kt
+++ app/src/main/java/com/guruthedev/instagram/ui/adapter/FeedPostAdapter.kt
@@ -8,29 +8,31 @@ import com.guruthedev.instagram.data.Post
 import com.guruthedev.instagram.databinding.PostListItemBinding
 
 class FeedPostAdapter : RecyclerView.Adapter<FeedPostAdapter.MyViewHolder>() {
-    private lateinit var binding: PostListItemBinding
     private var items = ArrayList<Post>()
     fun setUpdatedData(items: ArrayList<Post>) {
         this.items = items
         notifyDataSetChanged()
     }
 
-    class MyViewHolder(binding: PostListItemBinding) : RecyclerView.ViewHolder(binding.root)
+    class MyViewHolder(val binding: PostListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        binding = PostListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MyViewHolder(binding)
+        return MyViewHolder(
+            PostListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val post = items[position]
-        val fav = binding.HeartIcon
-        var isFav = false
-
-        binding.instagramPost = post
-        fav.setOnClickListener {
-            fav.setImageResource(R.drawable.fav_fill)
-            isFav = true
+        holder.binding.apply {
+            instagramPost = post
+            heartIcon.setOnClickListener {
+                heartIcon.setImageResource(R.drawable.fav_fill)
+            }
         }
     }
 
