4c9d4271c23820fd89d2f513b67a375376f1e3f5
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-01-17 20:18:01 +0530

    ViewModelFactory.kt class

---
+++ app/src/main/java/com/guruthedev/instagram/repository/ViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.guruthedev.instagram.repository
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class ViewModelFactory @Inject
+constructor(private val viewModelMap: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = viewModelMap[modelClass]
+            ?: viewModelMap.asIterable().firstOrNull { modelClass.isAssignableFrom(it.key) }?.value
+            ?: throw IllegalArgumentException("unknown model class $modelClass")
+        return try {
+            creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
